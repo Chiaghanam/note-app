e9ddef6e3247a943a09e8b3dd5c6c2325eb47302
Amobi <leofazzy98@gmail.com>
2026-03-01 11:38:04 +0100

    fix array error in home

--- react-note-app/src/pages/Home.jsx
+++ react-note-app/src/pages/Home.jsx
@@ -1,39 +1,38 @@
-import React from 'react'
-// import Filter from '../components/Filter'
+import React, { useEffect, useState } from 'react'
 import NoteCardContainer from '../components/NoteCardContainer'
-import { useEffect, useState } from 'react'
 import api from '../api'
 
 const Home = ({ searchtext }) => {
-
   const [notes, setNotes] = useState([])
+
   useEffect(() => {
-        api.get('/note_list')
-        .then(response => {
-            console.log(response.data);
-            setNotes(response.data)
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        })
-    }, [])
-
-    useEffect(()=>{
-      // if({searchtext} < 2 ) return;
-      api.get(`/search/?search=${searchtext}/`)
-      .then(res =>{
-        console.log(res.data)
-       
+    api.get('/note_list/')  
+      .then(response => {
+        console.log(response.data);
+        setNotes(Array.isArray(response.data) ? response.data : [])
       })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      })
+  }, [])
 
-    },[searchtext])
+  useEffect(() => {
+    if (!searchtext || searchtext.length < 2) return;  
+    api.get(`/search/?search=${searchtext}`)  
+      .then(res => {
+        console.log(res.data)
+        setNotes(Array.isArray(res.data) ? res.data : []) 
+      })
+      .catch(error => {
+        console.error('Search error:', error);
+      })
+  }, [searchtext])
+
   return (
     <div>
-        {/* <Filter  /> */}
-        <NoteCardContainer notes={notes} searchtext={searchtext} />
+      <NoteCardContainer notes={notes} searchtext={searchtext} />
     </div>
   )
 }
 
 export default Home
-
